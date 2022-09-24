364f8430d25c87fbba29314fe6a4046921053f78
Wilsonpaess <wilson_juh@hotmail.com>
2022-09-24 01:42:12 -0300

    .

---
+++ exercicios/ex008/exe08.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log(`A velocidade do seu carro é de ${vel} km/h`);
+if (vel > 60.5) {
+    console.log('Você ultrapassou o limite de velocidade. MULTADO')
+}
+
+console.log('Dirija sempre com cuidado')
+

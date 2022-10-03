55cb673e398410bf68de7b0f23f25b1785f032f5
Wilsonpaess <wilson_juh@hotmail.com>
2022-10-03 14:21:45 -0300

    .

--- exercicios/modelo de exercicio/script.js
+++ exercicios/modelo de exercicio/script.js
@@ -1,15 +1,47 @@
-function verificar() {
-    var data = new Date
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fano.value.lenght == 0 || Number(fano.value) > ano){
-        alert('ERRO!')
+let num = document.getElementById('fnum')
+let flista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-        var fsex = document.getElementById('radsex')
-        var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade} anos`
-
+        return false
     }
-
 }
+
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else{
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+          valores.push(Number(num.value))
+          let item = document.createElement('option')
+          item.text = `Valor ${num.value} foi adicionado`
+          flista.appendChild(item)
+    } else {
+        alert('Valor não indentificado')
+    }
+}
+
+num,value = ''
+num.focus()
+
+function finalizar (){
+    if (valores.length == 0) {
+        alert('Por favor digite valores')
+    } else {
+        let tot = valores.length
+        res.innerHTML - ''
+        res.innerHTML +=`<p> Ao todo temos ${tot} números cadastrados </p>`
+    }
+}
+
+
+

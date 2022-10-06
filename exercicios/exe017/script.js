138f16def888937b952efb5cdda7190db6bb2fb6
Wilsonpaess <wilson_juh@hotmail.com>
2022-10-06 08:04:56 -0300

    exe danki code

---
+++ exercicios/exe017/script.js
@@ -0,0 +1,25 @@
+/*var nome = "wilson"
+var idade = 25
+
+if(nome == "wilson" && idade == 25){
+    console.log('true')
+}else{
+    console.log('false')
+}*/
+
+/*var nome = "boniro"
+var pais = "brasil"
+
+if(nome == "boniro" && pais == "alemanha"){
+    console.log('votou')
+
+}else {
+    console.log('estrangeiro')
+}*/
+
+var numero = 50
+if(numero >= 50){
+    console.log('velho')
+} else {
+    console.log('novo')
+}

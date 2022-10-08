7b0d8df50c09380e546ace33e519f751f32af02f
Wilsonpaess <wilson_juh@hotmail.com>
2022-10-08 10:45:45 -0300

    .

---
+++ exercicios/exe019/script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Qual é seu nome?")
+var idade = prompt("Qual é sua idade?")
+var cidade = prompt("Qual é sua cidade?")
+
+console.log("O nome dele é: "+nome)
+console.log("A idade dele é: "+idade)
+console.log("A cidade dele é: "+cidade)

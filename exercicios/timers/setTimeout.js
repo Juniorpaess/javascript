266fa5dc5fba44460b6becc087a7ecf46896449b
Wilsonpaess <wilson_juh@hotmail.com>
2022-11-10 16:03:14 -0300

    utilizando timeouts

---
+++ exercicios/timers/setTimeout.js
@@ -0,0 +1,13 @@
+//const time = 3000
+//const finished = () => console.log('done!')
+
+//let timer = setTimeout(finished, time)
+//clearTimeout(timer)
+
+// Utilizando o setInterval
+
+const timeOut = 1000
+const checking = () => console.log('checking')
+
+setInterval(checking, timeOut)
+

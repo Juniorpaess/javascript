b9090430272affff75b0199bdfc1630bea896632
Wilsonpaess <wilson_juh@hotmail.com>
2022-11-11 13:15:37 -0300

    events

---
+++ exercicios/events/event.js
@@ -0,0 +1,17 @@
+const {inherits} = require('util')
+const {EventEmitter} = require('events')
+
+function Character(name){
+    this.name = name
+}
+
+inherits(Character, EventEmitter)
+
+const chapolin = new Character('Chapolin')
+
+chapolin.on('help', () => {
+    console.log(`Eu! O ${chapolin.name} colorado`)
+})
+
+console.log('Oh! E agora quem ira me defender?')
+chapolin.emit('help')

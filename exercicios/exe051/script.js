df6d7195a4a05d80213581068aa4e45c5fec4a2f
Wilsonpaess <wilson_juh@hotmail.com>
2022-11-17 17:34:51 -0300

    .

---
+++ exercicios/exe051/script.js
@@ -0,0 +1,15 @@
+class Poligono{
+    constructor(altura, largura){
+        this.altura = altura
+        this.largura = largura
+    }
+    get area(){
+        return this.#calcularArea()
+    }
+    #calcularArea(){
+        return this.altura * this.largura
+    }
+}
+
+let poligono = new Poligono(50, 60)
+console.log(poligono.area)
